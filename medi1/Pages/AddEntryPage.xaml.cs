dd68f730f5da4e8e1aefd867e6c32cf1a8cfa7ca
frangordon1 <f.gordon.22@unimail.winchester.ac.uk>
2025-04-17 11:41:51 +0100

    Add Activity Front End

--- medi1/Pages/AddEntryPage.xaml.cs
+++ medi1/Pages/AddEntryPage.xaml.cs
@@ -23,7 +23,22 @@ namespace medi1.Pages
                 EventNameContainer.IsVisible = true;
                 DateContainer.IsVisible = true;
                 HealthRelationContainer.IsVisible = true;
-                ImpactSelecter.IsVisible = true; 
+                ImpactSelecter.IsVisible = true;
+                activityTimingContainer.IsVisible = false;
+                IntensitySelecter.IsVisible = false;
+                aggravationContainer.IsVisible = false; 
+                NotesContainer.IsVisible = true;
+                ConfirmEntryButton.IsVisible = true;
+            }
+            else if (entryType == "Log Activity")
+            {
+                EventNameContainer.IsVisible = true;
+                DateContainer.IsVisible = false;
+                HealthRelationContainer.IsVisible = false;
+                ImpactSelecter.IsVisible = false;
+                activityTimingContainer.IsVisible = true;
+                IntensitySelecter.IsVisible = true;
+                aggravationContainer.IsVisible = true;
                 NotesContainer.IsVisible = true;
                 ConfirmEntryButton.IsVisible = true;
             }
@@ -51,6 +66,12 @@ namespace medi1.Pages
 
         }
 
+        private void AggravatedConfirmed(object sender, CheckedChangedEventArgs e)
+        {
+            //If Checkbox is checked, make condition picker visible
+            whichConditionContainer.IsVisible = e.Value;
+        }
+
         private void HealthEventPicker_SelectedIndexChanged(object sender, EventArgs e)
         {
             string healthEvent = HealthEventPicker.SelectedItem?.ToString();
@@ -68,70 +89,90 @@ namespace medi1.Pages
 
         private async void OnConfirmEntryClicked(object sender, EventArgs e)
         {
-            string healthEvent = HealthEventPicker.SelectedItem?.ToString();
-
+            // COMMON DATA //
+            //--------------- Entry Type ----------------------//
+            string entryType = EntrySelecter.SelectedItem.ToString();
             //------------------- Logging Name -----------------//
-            string eventName = NameEntry.Text.ToString();
-            
-            // ------------- Logging Duration ------------- //
-            string dateRange = DateSelecter.SelectedItem?.ToString();
-            if (dateRange == "Single Date" && FullDayCheck.IsChecked == true)
-            {
-                string eventDate = entryDatePicker.Date.ToString("d");
-                string eventDuration = "24";
+            string entryName = NameEntry.Text.ToString();
+            //------------------- Entry Notes -----------------------//
+            string entryNotes = NotesEntry.Text.ToString();
 
-            } 
-            else if (dateRange == "Single Date" && FullDayCheck.IsChecked == false) 
-            {
-                string eventDate = entryDatePicker.Date.ToString("d");
+            if (entryType == "Log Health Event"){
+                // ------------- Logging Duration ------------- //
+                string dateRange = DateSelecter.SelectedItem?.ToString();
+                 //------------------ Health Event Relation -----------//
+                string healthEvent = HealthEventPicker.SelectedItem?.ToString();
+                //------------ Event Impact -----------------//
+                string eventImpact = ImpactSelecter.SelectedItem?.ToString();
 
-                int hours = HourSpinner.Value;   // assuming your NumericSpinner has a Value property
-                int minutes = MinuteSpinner.Value;
-                double totalTime = hours + (minutes/60);
-                int totalTimeHours = (int)Math.Floor(totalTime);
-                string eventDuration = totalTimeHours.ToString();
-                //save duration to ConditionLog in database
+                if (dateRange == "Single Date" && FullDayCheck.IsChecked == true)
+                {
+                    string eventDate = entryDatePicker.Date.ToString("d");
+                    string eventDuration = "24";
+
+                } 
+                else if (dateRange == "Single Date" && FullDayCheck.IsChecked == false) 
+                {
+                    string eventDate = entryDatePicker.Date.ToString("d");
+
+                    int hours = HourSpinner.Value;   // assuming your NumericSpinner has a Value property
+                    int minutes = MinuteSpinner.Value;
+                    double totalTime = hours + (minutes/60);
+                    int totalTimeHours = (int)Math.Floor(totalTime);
+                    string eventDuration = totalTimeHours.ToString();
+                    //save duration to ConditionLog in database
+
+                }
+                else
+                {
+                    DateTime eventStartDate = StartDatePicker.Date;
+                    DateTime eventEndDate = EndDatePicker.Date;
+                    TimeSpan durationDays = eventEndDate - eventStartDate;
+                    int totalDuration = durationDays.Days;
+                    string eventDurationDays = totalDuration.ToString();
+                    //save to ConditionLog in database
+                }
+
+                // ---------------- LOGGING RELATIONSHIP TO EXISTING HEALTH ---------------//
+
+                if (healthEvent == "Independent Health Evemt")
+                {
+                    //Add to database container ConditionLogs
+                }
+                else if (healthEvent == "Related to Condition")
+                {
+                    AssociatedConditionSelecter.IsVisible = true;
+                    //Once condition selected and entry confirmed, entry is added to that Condition in the database
+                }
+                else if (healthEvent == "Related to Menstrual Cycle")
+                {
+                    //Add to Menstrual Cycle database contrainer
+                }
+                else if (healthEvent == "Part of a New Condition")
+                {
+                    //Open new condition page
+                }
+
+
+            } else if (entryType == "Log Activity")
+            {
+                //-------------Date and Duration----------//
+                string activityDate = activityDatePicker.Date.ToString("d");
+                int hours = AHourSpinner.Value;  
+                int minutes = AMinuteSpinner.Value;
+                int activityDuration = minutes + (hours*60);
+
+                //----------------Intensity-------------------//
+                string activityIntensity = IntensitySelecter.SelectedItem?.ToString();
+                
+                //-----------------Aggravated Condition-----------//
+                string aggedCondition = ConditionSelecter.SelectedItem?.ToString();
+                //Log name of activity in "triggers" of the names health condition
 
             }
-            else
-            {
-                DateTime eventStartDate = StartDatePicker.Date;
-                DateTime eventEndDate = EndDatePicker.Date;
-                TimeSpan durationDays = eventEndDate - eventStartDate;
-                int totalDuration = durationDays.Days;
-                string eventDurationDays = totalDuration.ToString();
-                //save to ConditionLog in database
-            }
-
-            // ---------------- LOGGING RELATIONSHIP TO EXISTING HEALTH ---------------//
-
-            if (healthEvent == "Independent Health Evemt")
-            {
-                //Add to database container ConditionLogs
-            }
-            else if (healthEvent == "Related to Condition")
-            {
-                AssociatedConditionSelecter.IsVisible = true;
-                //Once condition selected and entry confirmed, entry is added to that Condition in the database
-            }
-            else if (healthEvent == "Related to Menstrual Cycle")
-            {
-                //Add to Menstrual Cycle database contrainer
-            }
-            else if (healthEvent == "Part of a New Condition")
-            {
-                //Open new condition page
-            }
-
-            //---------------------LOGGING IMPACT----------------------//
-            string eventImpact = ImpactSelecter.SelectedItem?.ToString();
-
-            //------------------- NOTES -----------------------//
-            string eventNotes = NotesEntry.Text.ToString();
-            
             // Handle task addition logic here
-            await Navigation.PopModalAsync(); // Close the popup page
-        }
+                await Navigation.PopModalAsync(); // Close the popup page
+        } 
 
 
         private async void OnCancelEntryClicked(object sender, EventArgs e)
