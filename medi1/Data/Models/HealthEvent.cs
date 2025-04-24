423a31b58487849e84355465c41c0b259b213be2
frangordon1 <f.gordon.22@unimail.winchester.ac.uk>
2025-04-24 13:16:49 +0100

    health event change

--- medi1/Data/Models/HealthEvent.cs
+++ medi1/Data/Models/HealthEvent.cs
@@ -6,14 +6,14 @@ namespace medi1.Data.Models
     public class HealthEvent
     {
         public string Id { get; set; }
-        public string ConditionId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public string Duration { get; set; }
 
-        public int Severity { get; set; }  // 0-10 scale
-        public List<string> Symptoms { get; set; } = new();
-        public List<string> MedicationsTaken { get; set; } = new();
-        public List<string> Triggers { get; set; } = new();
+        public string RelatedCondition { get; set; }
+
+        public int Impact { get; set; }  // 0-10 scale
+
         public string Notes { get; set; }
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
 
