c4dbd5cf49c50f2700c7ffd143ae1a64c3e488fe
semaerensoyy <s.erensoy.22@unimail.winchester.ac.uk>
2025-05-09 16:07:10 +0100

    x

--- medi1/Pages/SettingsPage.xaml.cs
+++ medi1/Pages/SettingsPage.xaml.cs
@@ -7,6 +7,17 @@ namespace medi1.Pages
         public SettingsPage()
         {
             InitializeComponent();
+
+            // Initialize switch state from current theme
+            DarkModeSwitch.IsToggled = Application.Current.UserAppTheme == AppTheme.Dark;
+        }
+
+        private void DarkModeSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            // Set the app theme
+            Application.Current.UserAppTheme = e.Value
+                ? AppTheme.Dark
+                : AppTheme.Light;
         }
     }
 }
