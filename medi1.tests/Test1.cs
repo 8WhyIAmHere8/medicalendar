211fb88bb85d3275cb44f5ae361966f19a9a1850
frangordon1 <f.gordon.22@unimail.winchester.ac.uk>
2025-05-15 16:54:14 +0100

    Fran testing

--- medi1.tests/Test1.cs
+++ medi1.tests/Test1.cs
@@ -1,43 +1,70 @@
 using medi1.Data;
 using medi1.Data.Models;
 using medi1.Pages;
+using Microsoft.EntityFrameworkCore;
 using Moq;
-using Xunit;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.Xaml;
-using Microsoft.Maui.Graphics;
+using Xunit;
+using System.Windows.Input; // Only if ICommand is used
 
-public class Test1
+public class TaskEntryViewModelTests
 {
     private readonly Mock<MedicalDbContext> _mockDbContext;
-    private readonly HomePage _homePage;
-    private readonly List<CalendarTask> mockTaskLog = new List<CalendarTask>();
+    private readonly Mock<DbSet<CalendarTask>> _mockTaskLog;
+    private readonly List<CalendarTask> _taskList;
+    private readonly TaskEntryViewModel _viewModel;
 
-    public Test1()
+    public TaskEntryViewModelTests()
     {
+        _taskList = new List<CalendarTask>();
+        var queryable = _taskList.AsQueryable();
+
+        _mockTaskLog = new Mock<DbSet<CalendarTask>>();
+        _mockTaskLog.As<IQueryable<CalendarTask>>().Setup(m => m.Provider).Returns(queryable.Provider);
+        _mockTaskLog.As<IQueryable<CalendarTask>>().Setup(m => m.Expression).Returns(queryable.Expression);
+        _mockTaskLog.As<IQueryable<CalendarTask>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+        _mockTaskLog.As<IQueryable<CalendarTask>>().Setup(m => m.GetEnumerator()).Returns(() => _taskList.GetEnumerator());
+
+        _mockTaskLog.Setup(m => m.AddAsync(It.IsAny<CalendarTask>(), default))
+            .Returns((CalendarTask task, System.Threading.CancellationToken _) =>
+            {
+                _taskList.Add(task);
+                return ValueTask.FromResult((Microsoft.EntityFrameworkCore.ChangeTracking.EntityEntry<CalendarTask>)null);
+            });
+
         _mockDbContext = new Mock<MedicalDbContext>();
-        var mockTask = new medi1.Data.Models.CalendarTask { id = "1", TaskId = "1", Description = "Test", CompletionStatus = false };
+        _mockDbContext.Setup(db => db.TaskLog).Returns(_mockTaskLog.Object);
+        _mockDbContext.Setup(db => db.SaveChangesAsync(default)).ReturnsAsync(1);
 
-        _mockDbContext.Setup(db => db.TaskLog.Add(It.IsAny<CalendarTask>()))
-                      .Callback<CalendarTask>(mockTask => mockTaskLog.Add(mockTask));
-
-        _homePage = new HomePage(_mockDbContext.Object)
-        {
-            BindingContext = null
-        };
-
+        _viewModel = new TaskEntryViewModel(_mockDbContext.Object);
     }
 
     [Fact]
-    public async Task OnConfirmTaskClicked_ShouldAddTask()
+    public async Task ConfirmTaskAsync_ShouldAddTaskToDb_WhenDescriptionIsValid()
     {
-        // Act
-        _homePage.OnConfirmTaskClicked(null, null);
+        // Arrange
+        _viewModel.TaskDescription = "Test Task";
+        bool alertWasShown = false;
 
-        // Assert: Check if a blank task was added
-        Assert.Single(mockTaskLog);
-        Assert.Equal("Test", mockTaskLog[0].Description);
+        _viewModel.DisplayAlertRequested += (title, message, cancel) =>
+        {
+            alertWasShown = true;
+            return Task.CompletedTask;
+        };
+
+        // Act
+        var task = (Task)_viewModel.GetType()
+            .GetMethod("ConfirmTaskAsync", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+            .Invoke(_viewModel, null);
+
+        await task;
+
+
+        // Assert
+        Assert.Single(_taskList);
+        _mockDbContext.Verify(db => db.SaveChangesAsync(default), Times.Once);
+        Assert.False(alertWasShown);
     }
 }
